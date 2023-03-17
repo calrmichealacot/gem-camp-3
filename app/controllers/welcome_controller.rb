348b25e7c0f196e9d432cb6f66da50c6e536a03f
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-03-17 15:25:29 +0800

    Ruby webpage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
-class WelcomeController < ApplicationController
-  def index
+# app/controllers/welcome_controller.rb
+
+  class WelcomeController < ApplicationController
+    def index
+    end
+    def index; end
   end
-end
